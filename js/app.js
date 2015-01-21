a03c0c620961f813d933b13130b5ad00730ac7a8
Alejandro Nanez <alejonanez@gmail.com>
2015-01-20 19:55:12 -0500

    Add SemanticUI and did basic rendering

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-App = Ember.Application.create();
+EmberHub = Ember.Application.create();
 
-App.Router.map(function() {
+EmberHub.Router.map(function() {
 // put your routes here
+});
+
+EmberHub.IndexRoute = Ember.Route.extend({
+    model: function () {
+        return [
+            'User one',
+            'User two',
+            'User three',
+            'User four'
+        ];
+    }
+});
+
+EmberHub.IndexController = Ember.ArrayController.extend({
+    actions: {
+        clicked: function (name) {
+            alert('You clicked: ' + name);
+        }
+    },
+    newDate: function () {
+        return new Date();
+    }.property()
 });
