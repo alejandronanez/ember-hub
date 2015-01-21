eea7979b27676e9afa0fce0f020579bf5a03bc7d
Alejandro Nanez <alejonanez@gmail.com>
2015-01-20 23:21:21 -0500

    Basic profile added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 EmberHub = Ember.Application.create();
 
 var devs = [
-    { name: 'alejandronanez', fullName: 'Alejandro Nanez' },
-    { name: 'locke189', fullName: 'Juan Insuasti' },
-    { name: 'luiskdx', fullName: 'Luis Carlos' },
-    { name: 'tj', fullName: 'TJ' }
+    { login: 'alejandronanez', name: 'Alejandro Nanez' },
+    { login: 'locke189', name: 'Juan Insuasti' },
+    { login: 'luiskdx', name: 'Luis Carlos' },
+    { login: 'tj', name: 'TJ' }
 ];
 
 EmberHub.Router.map(function() {
-    this.resource('users', {path: '/users/:name'});
+    this.resource('user', {path: '/users/:login'});
 });
 
 EmberHub.IndexRoute = Ember.Route.extend({
     model: function () {
         return devs;
+    }
+});
+
+EmberHub.UserRoute = Ember.Route.extend({
+    model: function (params) {
+        return Ember.$.getJSON('https://api.github.com/users/' + params.login);
     }
 });
